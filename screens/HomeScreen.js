46c0532bc9de19473e1bcee9890d12e0ce6f5b01
MaitriRatanpara <98280891+MaitriRatanpara@users.noreply.github.com>
2023-05-06 12:41:32 -0400

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+
+export default class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+            </View>
+        )
+    }
+};
